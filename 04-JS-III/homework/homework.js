20439b01bdb00be8dc72a9645d7e273ea922a8d9
FelipeBussio <bussiofelipe@gmail.com>
2022-03-17 16:01:13 -0300

    devolver

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -1,7 +1,7 @@
 // No cambies los nombres de las funciones.
 
-function devolverPrimerElemento(array) {
-  var array=[1,2,3,4]
+function devolverPrimerElemento( array =[1,2,3,4] ) {
+  
   array [0]
   // Devuelve el primer elemento de un  array (pasado por parametro)
   // Tu código:
