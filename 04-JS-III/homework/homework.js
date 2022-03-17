1715f7a3a5aa6c4250ea15ef4e576b5ce33928dc
FelipeBussio <bussiofelipe@gmail.com>
2022-03-17 16:07:27 -0300

    last

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -9,6 +9,7 @@ function devolverPrimerElemento( array ) {
 
 
 function devolverUltimoElemento(array) {
+  return array [array.length-1]
   // Devuelve el último elemento de un array
   // Tu código:
 }
